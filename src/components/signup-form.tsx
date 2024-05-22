f7a0c294dc6e5c053ef7ae7491d4411498b9b623
luccaparadeda <luccaparadedaprofessional@gmail.com>
2024-05-21 23:22:23 -0300

    creating forms and pdf visualizar

---
+++ src/components/signup-form.tsx
@@ -0,0 +1,113 @@
+"use client";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { type FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+	email: z
+		.string()
+		.email("Seu email deve ser similar a esse exemplo nome@exemplo.extensão"),
+	name: z
+		.string({ required_error: "" })
+		.min(2, "Seu nome deve ter pelo menos 2 letras"),
+	lastName: z
+		.string({ required_error: "" })
+		.min(2, "Seu sobrenome deve ter pelo menos 2 letras"),
+});
+
+type FormValues = z.infer<typeof schema>;
+
+export function SignUpForm() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormValues>({
+		resolver: zodResolver(schema),
+	});
+
+	const onSubmit = (data: FormValues) => {
+		console.log(data);
+	};
+	return (
+		<Card className="max-w-sm">
+			<CardHeader>
+				<CardTitle className="text-xl">Cadastre-se</CardTitle>
+				<CardDescription>
+					Insira suas informações para se cadastrar
+				</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<form onSubmit={handleSubmit(onSubmit)} className="grid gap-4">
+					<div className="grid grid-cols-2 gap-4">
+						<div className="grid gap-2">
+							<Label htmlFor="first-name">Nome*</Label>
+							<div className="grid gap-1">
+								<Input
+									id="first-name"
+									placeholder="Pedro"
+									{...register("name")}
+								/>
+								{errors.name?.message && (
+									<p className="text-red-500 text-xs min-h-2">
+										{errors.name?.message}
+									</p>
+								)}
+							</div>
+						</div>
+						<div className="grid gap-2">
+							<Label htmlFor="last-name">Sobrenome*</Label>
+							<div className="grid gap-1">
+								<Input
+									id="last-name"
+									placeholder="Oliveira"
+									{...register("lastName")}
+								/>
+								{errors.lastName?.message && (
+									<p className="text-red-500 text-xs min-h-2">
+										{errors.lastName?.message}
+									</p>
+								)}
+							</div>
+						</div>
+					</div>
+					<div className="grid gap-2">
+						<Label htmlFor="email">Email*</Label>
+						<div className="grid gap-1">
+							<Input
+								id="email"
+								type="email"
+								placeholder="m@exemplo.com"
+								{...register("email")}
+							/>
+							{errors.email?.message && (
+								<p className="text-red-500 text-xs min-h-2">
+									{errors.email?.message}
+								</p>
+							)}
+						</div>
+					</div>
+					<Button type="submit" className="w-full">
+						Quero receber o conteúdo
+					</Button>
+				</form>
+				<div className="mt-4 text-center text-xs">
+					Não se preocupe, caso você já tenha feito cadastro anteriormente é só
+					inserir o email novamente <span className="text-primary">{"<3"}</span>
+				</div>
+			</CardContent>
+		</Card>
+	);
+}
