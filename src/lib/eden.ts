08e272dfc7af2f34be4cc71d91e4b37fe36a3be0
luccaparadeda <luccaparadedaprofessional@gmail.com>
2024-05-25 21:34:01 -0300

    fix: eden url

--- src/lib/eden.ts
+++ src/lib/eden.ts
@@ -2,5 +2,5 @@ import { treaty } from "@elysiajs/eden";
 import type { App } from "../app/api/[[...slugs]]/route";
 
 export const eden = treaty<App>(
-	`${process.env.URL ?? "http://localhost"}:3000`,
+	`${process.env.URL ?? "http://localhost:3000"}`,
 );
